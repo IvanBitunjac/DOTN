b16ad703af79cb7a8b84ab014d8a135300516d66
mmaini <mariza.maini@itsistemi.com>
2022-04-27 14:29:26 +0200

    WCF

---
+++ Predavanje8Wcf/SampleWcfWebService/App_Code/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWcfWebService
+{
+    [ServiceContract]
+    public interface IUser
+    {
+        [OperationContract]
+        string WelcomeUser(string username);
+        [OperationContract]
+        bool AuthenticateUser(string username, string password);
+    }
+
+}
---
+++ Predavanje8Wcf/SampleWcfWebService/App_Code/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWcfWebService
+{
+    public class User : IUser
+    {
+        public bool AuthenticateUser(string username, string password)
+        {
+            if (username == "wcf" && password == "test")
+                return true;
+
+            return false;
+        }
+
+        public string WelcomeUser(string username)
+        {
+            return "Welcome " + username;
+        }
+    }
+}
