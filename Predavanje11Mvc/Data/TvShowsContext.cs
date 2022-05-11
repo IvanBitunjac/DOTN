bef4dbab4e6f8a69f501b74dc074cb2c5fa95ca2
mmaini <mariza.maini@itsistemi.com>
2022-05-11 18:26:14 +0200

    Mvc Tv Shows

---
+++ Predavanje11Mvc/Data/TvShowsContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Predavanje11Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Predavanje11Mvc.Data
+{
+    public class TvShowsContext : DbContext
+    {
+        public TvShowsContext(DbContextOptions<TvShowsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TvShow> TvShow { get; set; }
+    }
+}
